6659ed3eef531eb8281e87e6c3222a611f1222d3
Logan Ferris <FLogan800@gmail.com>
2024-12-14 17:05:04 -0600

    Complete day 5.2

--- 2024/day_5/src/main.rs
+++ 2024/day_5/src/main.rs
@@ -11,7 +11,9 @@ fn main() {
     for rule in split[0].split_whitespace() {
         let start_end = rule.split("|").collect::<Vec<&str>>();
         let first_page: i32 = start_end[0].parse().expect("Failed to parse starting page");
-        let second_page: i32 = start_end[1].parse().expect("Failed to parse the second page");
+        let second_page: i32 = start_end[1]
+            .parse()
+            .expect("Failed to parse the second page");
 
         rules.push((first_page, second_page));
     }
@@ -25,26 +27,56 @@ fn main() {
         }
     }
 
-    let mut sum = 0;
+    let mut valid_sum = 0;
+    let mut invalid_sum = 0;
 
-    for update in &updates {
-        let mut is_valid = true;
-        for rule in &rules {
-            if !validate_update(update, rule) {
-                is_valid = false;
+    for update in &mut updates {
+        let mut properly_ordered = true;
+
+        while !is_valid(update, &rules) {
+            properly_ordered = false;
+
+            for rule in &rules {
+                let first_page_index = match update.iter().position(|&r| r == rule.0) {
+                    Some(index) => index,
+                    None => continue,
+                };
+
+                let second_page_index = match update.iter().position(|&r| r == rule.1) {
+                    Some(index) => index,
+                    None => continue,
+                };
+
+                if !validate_rule(update, rule) {
+                    update.swap(first_page_index, second_page_index);
+                }
             }
         }
 
-        if is_valid {
-            let middle_index = update.len() / 2;
-            sum += update[middle_index];
+        if properly_ordered {
+            valid_sum += update[update.len() / 2];
+        } else {
+            invalid_sum += update[update.len() / 2];
         }
     }
 
-    println!("Sum: {sum}");
+    println!("Valid Sum: {valid_sum}");
+    println!("Invalid Sum: {invalid_sum}");
 }
 
-fn validate_update(update: &Vec<i32>, rule: &(i32, i32)) -> bool{
+fn is_valid(update: &Vec<i32>, rules: &Vec<(i32, i32)>) -> bool {
+    let mut is_valid = true;
+
+    for rule in rules {
+        if !validate_rule(update, rule) {
+            is_valid = false;
+        }
+    }
+
+    return is_valid;
+}
+
+fn validate_rule(update: &Vec<i32>, rule: &(i32, i32)) -> bool {
     let first_page_index = match update.iter().position(|&r| r == rule.0) {
         Some(index) => index,
         None => return true,
@@ -56,8 +88,8 @@ fn validate_update(update: &Vec<i32>, rule: &(i32, i32)) -> bool{
     };
 
     if first_page_index < second_page_index {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
